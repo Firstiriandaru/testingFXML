cf334a55af0ebff237a89e47ed6289f9b4b10536
Abhyasa Wira F. <118235698+Firstiriandaru@users.noreply.github.com>
2024-10-13 19:58:44 +0700

    test kedua fxml

---
+++ src/testing/FXMLPercobaanController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package testing;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Acer
+ */
+public class FXMLPercobaanController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
